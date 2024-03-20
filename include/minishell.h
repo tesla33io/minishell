6e58741e128773ba8fab90dd36014693394c7d7a
Lisamarie Treser <ltreser@c4c1c3.42berlin.de>
2024-03-20 17:00:35 +0100

    chore: add all headers to .h and add emojis to prompt

--- include/minishell.h
+++ include/minishell.h
@@ -6,12 +6,29 @@
 /*   By: astavrop <astavrop@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 13:27:59 by astavrop          #+#    #+#             */
-/*   Updated: 2024/03/14 13:28:03 by astavrop         ###   ########.fr       */
+/*   Updated: 2024/03/20 16:58:33 by ltreser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <<string.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <curses.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "../../lib/ft_printf/includes/ft_printf.h"
+#include "../../lib/libft/libft.h"
 
 int		render_prompt(void);
 
