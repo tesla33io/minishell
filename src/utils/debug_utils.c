9b52675d9bd28821e70f10c0ffda143715a1815b
astavrop <artemstavropolov@gmail.com>
2024-03-21 21:30:18 +0100

    chore: update utils.

--- src/utils/debug_utils.c
+++ src/utils/debug_utils.c
@@ -1,26 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   free_utils.c                                       :+:      :+:    :+:   */
+/*   debug_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: astavrop <astavrop@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/20 16:33:20 by astavrop          #+#    #+#             */
-/*   Updated: 2024/03/20 16:33:20 by astavrop         ###   ########.fr       */
+/*   Created: 2024/03/21 20:34:57 by astavrop          #+#    #+#             */
+/*   Updated: 2024/03/21 20:34:57 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/executor.h"
 #include <stdlib.h>
 
-void	free_str_list(char	**lst)
+t_SimpleCommand	*cmd_gen(char *bin, char **args, char **envp, char **in_fs,
+		char **out_fs, int pipefd[2])
 {
-	int	i;
+	t_SimpleCommand	*cmd;
 
-	i = -1;
-	if (!lst)
-		return ;
-	while (lst[++i])
-		free(lst[i]);
-	free(lst);
+	cmd = malloc(sizeof(t_SimpleCommand));
+	cmd->bin = bin;
+	cmd->args = args;
+	cmd->envp = envp;
+	cmd->in_fs = in_fs;
+	cmd->out_fs = out_fs;
+	cmd->pipefd[0] = pipefd[0];
+	cmd->pipefd[1] = pipefd[1];
+	return (cmd);
 }
