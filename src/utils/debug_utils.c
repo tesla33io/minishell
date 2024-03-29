a8fa925878754ec45fc1cdf934af72e2ed6f890c
astavrop <artemstavropolov@gmail.com>
2024-03-29 22:19:13 +0100

    chore: move cmd_print function to debug utils

--- src/utils/debug_utils.c
+++ src/utils/debug_utils.c
@@ -6,12 +6,13 @@
 /*   By: astavrop <astavrop@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 20:34:57 by astavrop          #+#    #+#             */
-/*   Updated: 2024/03/21 20:34:57 by astavrop         ###   ########.fr       */
+/*   Updated: 2024/03/29 21:02:36 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/executor.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 t_SimpleCommand	*cmd_gen(char *bin, char **args, char **envp, char **in_fs,
 		char **out_fs, int pipefd[2])
@@ -27,4 +28,18 @@ t_SimpleCommand	*cmd_gen(char *bin, char **args, char **envp, char **in_fs,
 	cmd->pipefd[0] = pipefd[0];
 	cmd->pipefd[1] = pipefd[1];
 	return (cmd);
+}
+
+void	print_command(t_SimpleCommand	*cmd)
+{
+	int	i;
+
+	if (cmd->bin)
+		printf("Binary: %s\n", cmd->bin);
+	if (cmd->args)
+	{
+		i = -1;
+		while (cmd->args[++i])
+			printf("Arg: %s\n", cmd->args[i]);
+	}
 }
