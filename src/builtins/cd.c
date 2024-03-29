bc3189a0b56e7edf77b352b2cea16d66bb1879b5
astavrop <artemstavropolov@gmail.com>
2024-03-29 18:35:36 +0100

    fix: replace `ft_putstr_fd`->`perror`

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,15 +6,15 @@
 /*   By: astavrop <astavrop@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 16:59:32 by astavrop          #+#    #+#             */
-/*   Updated: 2024/03/24 16:59:32 by astavrop         ###   ########.fr       */
+/*   Updated: 2024/03/29 18:36:00 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/builtins.h"
-#include "../../lib/libft/libft.h"
 
 #include <errno.h>
 #include <string.h>
+#include <stdio.h>
 #include <unistd.h>
 
 void	ft_cd(const char *path)
@@ -24,10 +24,10 @@ void	ft_cd(const char *path)
 	if (chdir(path) != 0)
 	{
 		errnum = errno;
-		ft_putstr_fd("cd: ", 2);
-		ft_putstr_fd(strerror(errnum), 2);
-		ft_putstr_fd(": ", 2);
-		ft_putstr_fd((char *)path, 2);
-		ft_putstr_fd("\n", 2);
+		perror("cd: ");
+		perror(strerror(errnum));
+		perror(": ");
+		perror((char *)path);
+		perror("\n");
 	}
 }
