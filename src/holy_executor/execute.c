e07c77c0f701f5abb04fe8bb3b08291904415791
astavrop <artemstavropolov@gmail.com>
2024-04-26 19:58:48 +0200

    chore!: reimplementaion of command execution

--- src/holy_executor/execute.c
+++ src/holy_executor/execute.c
@@ -3,124 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: astavrop <astavrop@student.42berlin.d      +#+  +:+       +#+        */
+/*   By: astavrop <astavrop@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/19 22:58:11 by astavrop          #+#    #+#             */
-/*   Updated: 2024/04/21 16:29:43 by astavrop         ###   ########.fr       */
+/*   Created: 2024/04/26 18:59:25 by astavrop          #+#    #+#             */
+/*   Updated: 2024/04/26 19:49:25 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h> /* pid_t, fork */
+
 #include "../../include/executor.h"
-#include "../../lib/libft/libft.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
+static void	setup_ipc(t_SimpleCommand *cmd);
 
-#define INVALID_CMD_ERR_MSG "Invalid command"
-
-/**
- * @brief Searches for the binary executable file in the directories listed
- *        in the PATH environment variable.
- *
- * This function searches for the binary executable file corresponding
- * to the given simple command in the directories listed in the PATH
- * environment variable. If the binary executable file is found in any of the
- * directories, the function `bin_check` checks that found file is an
- * executable. If the binary executable file is not found in any directory,
- * the function returns.
- *
- * @param cmd Pointer to the simple command structure containing the details 
- *        of the command.
+/*
+ * executes a command and
+ * returns an id of child process
  */
-void	find_bin(t_SimpleCommand *cmd)
-{
-	char	*env_path;
-	char	**path_list;
-	int		i;
-
-	env_path = getenv("PATH");
-	if (!env_path)
-		return ;
-	path_list = ft_split(env_path, ':');
-	if (!path_list)
-		return ;
-	i = -1;
-	while (path_list[++i])
-	{
-		if (bin_check(cmd, path_list[i]) == 0)
-		{
-			free_str_list(path_list);
-			return ;
-		}
-	}
-	free_str_list(path_list);
-}
-
-/**
- * @brief Sets up input and output redirection for a simple command,
- *        possibly involving pipes.
- *
- * This function sets up input and output redirection for a simple command,
- * taking into account whether it is part of the left or right command 
- * of a pipeline. It also handles the case when the command has its own 
- * file descriptors for input and output. The function redirects standard
- * input and output accordingly, allowing the command to receive input
- * from the previous command in the pipeline and send output to the
- * next command in the pipeline.
- *
- * @param cmd Pointer to the simple command structure containing the details
- *            of the command.
- * @param mode A character indicating the mode of the current pipe:
- *             - 'l' indicates that the current pipe is for the left command.
- *             - 'r' indicates that the current pipe is for the right command.
- */
-void	set_pipes(t_SimpleCommand *cmd, int mode)
-{
-	if (mode == 'l')
-	{
-		if (cmd->pipefd[RDEND] >= 0)
-			close(cmd->pipefd[RDEND]);
-		if (cmd->in_fd == -1 && cmd->pipefd[RDEND] >= 0)
-			dup2(cmd->pipefd[RDEND], STDIN_FILENO);
-	}
-	else if (mode == 'r')
-	{
-		if (cmd->pipefd[WREND] >= 0)
-			close(cmd->pipefd[WREND]);
-		if (cmd->in_fd == -1 && cmd->pipefd[RDEND] >= 0)
-			dup2(cmd->pipefd[RDEND], STDIN_FILENO);
-	}
-	if (cmd->in_fd != -1)
-		dup2(cmd->in_fd, STDIN_FILENO);
-	if (cmd->out_fd != -1)
-		dup2(cmd->out_fd, STDOUT_FILENO);
-	else if (cmd->out_fd == -1 && cmd->pipefd[WREND] >= 0)
-		dup2(cmd->pipefd[WREND], STDOUT_FILENO);
-}
-
-/**
- * @brief Executes a simple command.
- *
- * This function executes the given simple command in a child process.
- * It first forks a new process, then sets up input and output redirection
- * using the `set_pipes` function according to the specified mode. After
- * setting up the pipes, it checks if the command is valid using cmd_check.
- * If the command is valid, it searches for the binary executable file
- * using `find_bin`, prints the command details using print_command,
- * and attempts to execute the command using execve.
- * If the execution fails, it exits with an error message.
- *
- * @param cmd Pointer to the simple command structure containing the details
- *            of the command.
- * @param mode A character indicating the mode of the current pipe:
- *             - 'l' indicates that the current pipe is for the left command.
- *             - 'r' indicates that the current pipe is for the right command.
- * @return Returns the process ID (PID) of the child process.
- *         - Returns -1 if forking fails.
- */
-int	cmd_exe(t_SimpleCommand *cmd, int mode)
+int	execute_command(t_SimpleCommand *cmd)
 {
 	pid_t	pid;
 
@@ -129,13 +29,39 @@ int	cmd_exe(t_SimpleCommand *cmd, int mode)
 		fork_fail();
 	if (pid == 0)
 	{
-		set_pipes(cmd, mode);
-		if (cmd_check(cmd) != 0)
-			(perror(INVALID_CMD_ERR_MSG), exit(FAIL));
-		find_bin(cmd);
-		print_command(cmd);
-		if (execve(cmd->bin, cmd->args, cmd->envp) == -1)
-			exit(execution_fail(cmd->args[0]));
+		// check and look for binary
+		close(cmd->in_fd);
+		dup2(cmd->out_fd, STDOUT_FILENO);
+		if (execve(cmd->bin, cmd->args, cmd->envp) < 0)
+			execution_fail(cmd->args[0]);
 	}
 	return (pid);
 }
+
+/*
+ * setup Inter Process Comunication
+ *
+ * if pipefd[0] == 1 && pipefd[1] == 0
+ *		the command is first
+ * else if pipefd[1] == 1 && pipefd[0] == 1
+ *		the command is last
+ * else
+ *		the command is in the middle
+ *
+static void	setup_ipc(t_SimpleCommand *cmd)
+{
+	if (cmd.pipefd[0] == 1 && cmd->pipefd[1] == 0)
+	{
+		close(cmd->in_fd);
+		dup2(cmd->out_fd, STDOUT_FILENO);
+	}
+	else if (cmd.pipefd[1] == 1 && cmd.pipefd[0] == 0)
+	{
+
+	}
+	else
+	{
+	
+	}
+}
+*/
--- src/utils/execution_utils.c
+++ src/utils/execution_utils.c
@@ -5,76 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: astavrop <astavrop@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/30 13:48:01 by astavrop          #+#    #+#             */
-/*   Updated: 2024/04/21 16:31:51 by astavrop         ###   ########.fr       */
+/*   Created: 2024/04/26 18:59:58 by astavrop          #+#    #+#             */
+/*   Updated: 2024/04/26 19:58:10 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdbool.h>
+
 #include "../../include/executor.h"
-#include "../../lib/libft/libft.h"
 
-#include <stdlib.h>
-#include <unistd.h>
-
-/**
- * @brief Checks if the binary executable file specified in the
- *        simple command exists and is executable.
- *
- * This function constructs the full path to the binary executable file
- * using the provided directory path and the binary name from the given
- * simple command. It then checks if the constructed binary path exists and is
- * executable. If the binary executable file exists and is executable, it
- * updates the command structure with the full binary path and returns 0.
- * Otherwise, it frees the allocated memory and returns -1.
- *
- * @param cmd Pointer to the simple command structure containing the details
- *            of the command.
- * @param path The directory path where the binary executable file is
- *             expected to be located.
- * @return Returns 0 if the binary executable file exists and is executable.
- *         Returns -1 otherwise.
- */
-int	bin_check(t_SimpleCommand *cmd, char *path)
+bool	check_command(t_SimpleCommand **command)
 {
-	char	*bin_path;
-	char	*tmp_path;
+	t_SimpleCommand	*cmd;
 
-	tmp_path = ft_strjoin(path, "/");
-	bin_path = ft_strjoin(tmp_path, cmd->bin);
-	free(tmp_path);
-	if (access(bin_path, F_OK) == 0)
-	{
-		if (access(bin_path, X_OK) == 0)
-		{
-			cmd->bin = bin_path;
-			return (0);
-		}
-	}
-	free(bin_path);
-	return (-1);
-}
-
-/**
- * @brief Checks if the simple command is valid.
- *
- * This function checks if the simple command has a valid binary path
- * and arguments. It compares the binary path stored in the command structure
- * with the first argument of the command. If they match, it considers
- * the command valid and returns 0. Otherwise, it returns -1 to indicate
- * an invalid command.
- *
- * @param cmd Pointer to the simple command structure containing the details
- *            of the command.
- * @return Returns 0 if the simple command is valid.
- *         Returns -1 if the simple command is invalid.
- */
-int	cmd_check(t_SimpleCommand *cmd)
-{
-	if (!cmd->bin)
-		return (-1);
-	if (!cmd->args)
-		return (-1);
-	if (ft_strncmp(cmd->bin, cmd->args[0], ft_strlen(cmd->bin)) != 0)
-		return (-1);
-	return (0);
+	cmd = *(command);
+	if (!cmd->bin || !cmd->args || !cmd->envp)
+		return (false);
+	// binary lookup coming soon..................
+	return (false);
 }
