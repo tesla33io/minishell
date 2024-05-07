cc879665bdce11bff9e4932422c58f930e1b3a6d
astavrop <artemstavropolov@gmail.com>
2024-05-07 21:54:30 +0200

    chore: refactoring some builtins

--- include/builtins.h
+++ include/builtins.h
@@ -6,7 +6,7 @@
 /*   By: astavrop <astavrop@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 20:59:52 by astavrop          #+#    #+#             */
-/*   Updated: 2024/05/07 18:27:14 by astavrop         ###   ########.fr       */
+/*   Updated: 2024/05/07 21:52:03 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,11 @@
 # include "../lib/libft/libft.h"
 # include "executor.h"
 
-# define ENVP_HT_SIZE 1000
-# define BUILTIN_HT_SIZE 42
-
 void	ft_echo(t_SimpleCommand *cmd);
 void	ft_cd(t_SimpleCommand *cmd);
 void	ft_pwd(t_SimpleCommand *cmd);
 void	ft_env(t_SimpleCommand *cmd);
-void	ft_env_sorted(char *envp[]);
+void	ft_env_sorted(t_kv envp_ht[TABLE_SIZE]);
 // void	ft_export(char *key, char *value, char **envp[]);
 void	ft_export(t_SimpleCommand *cmd);
 // char	**ft_export(t_SimpleCommand *cmd);
@@ -35,7 +32,7 @@ void	ft_unset(char *name, char **envp[]);
 /* Utils */
 
 // char	**ft_envdup(char *envp[]);
-void	ft_envdup(t_kv envp_ht[ENVP_HT_SIZE], char *envp[]);
+void	ft_envdup(t_kv envp_ht[TABLE_SIZE], char *envp[]);
 void	free_envp(char *envp[]);
 char	*create_kv_entry(char *key, char *value);
 bool	envp_contains(char *key, char *envp[]);
--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: astavrop <astavrop@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 19:40:08 by astavrop          #+#    #+#             */
-/*   Updated: 2024/05/07 19:03:20 by astavrop         ###   ########.fr       */
+/*   Updated: 2024/05/07 21:51:42 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-static void	print_envp(char *envp[], bool for_export);
+static void	print_envp(char **keys, t_kv envp_ht[TABLE_SIZE], bool for_export);
 
 void	ft_env(t_SimpleCommand *cmd)
 {
 	if (!cmd->envp)
 		ft_putendl_fd("env: no environment variables", 2);
 	else
-		print_envp(cmd->envp, false);
+		// print_envp(, false);
 }
 
 char	*ft_getenv(char *envp[], char *name)
@@ -62,34 +62,30 @@ void	ft_envdup(t_kv envp_ht[ENVP_HT_SIZE], char *envp[])
 	}
 }
 
-void	ft_env_sorted(char *envp[])
+void	ft_env_sorted(t_kv envp_ht[TABLE_SIZE])
 {
-	char			**sorted_envp = NULL;
+	char			**sorted_keys;
 	unsigned int	i;
 
 	i = 0;
-	while (envp[i])
-		i++;
-	// sorted_envp = ft_envdup(NULL, envp);
-	ft_strquicksort((const char **) sorted_envp, i);
-	print_envp(sorted_envp, true);
-	ft_free_ptr(sorted_envp);
+	sorted_keys = get_all_keys(envp_ht);
+	ft_strquicksort((const char **) sorted_keys, i);
+	print_envp(sorted_keys, envp_ht, true);
 }
 
-static void	print_envp(char *envp[], bool for_export)
+static void	print_envp(char **keys, t_kv envp_ht[TABLE_SIZE], bool for_export)
 {
 	int	i;
 
 	i = 0;
-	while (envp[i] != NULL)
+	while (keys[i] != NULL)
 	{
-		if (ft_strchr(envp[i], '=') != NULL)
-		{
-			if (for_export)
-				ft_putstr_fd("export ", 1);
-			ft_putstr_fd(envp[i], 1);
-			ft_putstr_fd("\n", 1);
-		}
+		if (for_export)
+			ft_putstr_fd("export ", 1);
+		ft_putstr_fd(keys[i], 1);
+		ft_putstr_fd("=", 1);
+		ft_putstr_fd(ht_get(envp_ht, keys[i]), 1);
+		ft_putstr_fd("\n", 1);
 		i++;
 	}
 }
