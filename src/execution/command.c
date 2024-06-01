4c0e7e601af7c773ba8322d15bb70be3d71681a6
astavrop <artemstavropolov@gmail.com>
2024-06-01 18:22:54 +0200

    fix: improve IPC

--- src/execution/command.c
+++ src/execution/command.c
@@ -6,7 +6,7 @@
 /*   By: astavrop <astavrop@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 18:07:14 by astavrop          #+#    #+#             */
-/*   Updated: 2024/05/31 20:33:43 by astavrop         ###   ########.fr       */
+/*   Updated: 2024/06/01 18:12:51 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,47 @@
 #include <unistd.h>
 #include <stdlib.h> /* getenv() */
 
-void execute_command_in_child(t_Command *command, int pipefd[2][2], int i, int num_cmds) {
+static void	setup_ipc(t_Command *cmd, int i,int pipefd[2][2], int num_cmds);
+
+void execute_command_in_child(t_Command *command, int pipefd[2][2], int i, int num_cmds)
+{
 	char	*bin;
 
-    if (i > 0) {
-        dup2(pipefd[(i - 1) % 2][0], STDIN_FILENO);
-        close(pipefd[(i - 1) % 2][1]);
-    }
-    if (i < num_cmds - 1) {
-        dup2(pipefd[i % 2][1], STDOUT_FILENO);
-        close(pipefd[i % 2][0]);
-    }
+	setup_ipc(command, i, pipefd, num_cmds);
 
-    bin = find_executable_on_path(getenv("PATH"), command->bin_name);
+	bin = find_executable_on_path(getenv("PATH"), command->bin_name);
     printf("[%sDEBUG%s] - command to execute - %s\n", YEL, RESET, command->bin_name);
     print_cmd(command);
 
-    if (execve(bin, command->args, command->envpv) < 0) {
+    if (execve(bin, command->args, command->envpv) < 0)
         execve_fail();
-    }
+}
+
+static void	setup_ipc(t_Command *cmd, int i,int pipefd[2][2], int num_cmds)
+{
+	// Setup input redirection
+	if (cmd->in_fd != 0) {
+		dup2(cmd->in_fd, STDIN_FILENO);
+	} else if (i > 0) { 
+		dup2(pipefd[(i - 1) % 2][0], STDIN_FILENO);
+	}
+
+	// Close the read end of the previous pipe
+	if (i > 0) {
+		close(pipefd[(i - 1) % 2][0]);
+		close(pipefd[(i - 1) % 2][1]);
+	}
+
+	// Setup output redirection
+	if (cmd->out_fd != 1) {
+		dup2(cmd->out_fd, STDOUT_FILENO);
+	} else if (i < num_cmds - 1) { 
+		dup2(pipefd[i % 2][1], STDOUT_FILENO);
+	}
+
+	// Close the write end of the current pipe
+	if (i < num_cmds - 1) {
+		close(pipefd[i % 2][0]);
+		close(pipefd[i % 2][1]);
+	}
 }
--- src/execution/pipeline.c
+++ src/execution/pipeline.c
@@ -6,7 +6,7 @@
 /*   By: astavrop <astavrop@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 16:00:09 by astavrop          #+#    #+#             */
-/*   Updated: 2024/05/31 20:34:05 by astavrop         ###   ########.fr       */
+/*   Updated: 2024/06/01 18:22:07 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,49 +18,56 @@
 
 #include <stdio.h> /* perror() */
 
-int execute_pipeline(t_Pipeline *pipeline) {
-    pid_t *pids = malloc(sizeof(pid_t) * pipeline->num_cmds);
-    int pipefd[2][2];
-    int i;
+int execute_pipeline(t_Pipeline *pipeline)
+{
+	pid_t *pids = malloc(sizeof(pid_t) * pipeline->num_cmds);
+	int pipefd[2][2];
+	int i;
 
-    if (!pids) {
-        perror("malloc");
-        return -1;
-    }
+	if (!pids)
+	{
+		perror("malloc");
+		return -1;
+	}
 
-    for (i = 0; i < pipeline->num_cmds; i++) {
-        if (i < pipeline->num_cmds - 1) {
-            if (pipe(pipefd[i % 2]) == -1) {
-                perror("pipe");
-                return -1;
-            }
-        }
+	for (i = 0; i < pipeline->num_cmds; i++)
+	{
+		if (i < pipeline->num_cmds - 1)
+		{
+			if (pipe(pipefd[i % 2]) == -1)
+			{
+				perror("pipe");
+				return -1;
+			}
+		}
 
-        pids[i] = fork();
-        if (pids[i] < 0) {
-            perror("fork");
-            return -1;
-        } else if (pids[i] == 0) {
+		pids[i] = fork();
+		if (pids[i] < 0)
+		{
+			perror("fork");
+			return -1;
+		}
+		else if (pids[i] == 0)
+		{
 			if (i > 0)
 				close(pipefd[(i - 1) % 2][1]);
 			if (i < pipeline->num_cmds - 1)
 				close(pipefd[i % 2][0]);
-            execute_command_in_child(pipeline->commands[i], pipefd, i, pipeline->num_cmds);
-        }
+			execute_command_in_child(pipeline->commands[i], pipefd, i, pipeline->num_cmds);
+		}
 
-        if (i > 0) {
-            close(pipefd[(i - 1) % 2][0]);
-            close(pipefd[(i - 1) % 2][1]);
-        }
-    }
+		if (i > 0) {
+			close(pipefd[(i - 1) % 2][0]);
+			close(pipefd[(i - 1) % 2][1]);
+		}
+	}
 
-    close(pipefd[(i - 1) % 2][0]);
-    close(pipefd[(i - 1) % 2][1]);
+	close(pipefd[(i - 1) % 2][0]);
+	close(pipefd[(i - 1) % 2][1]);
 
-    for (i = 0; i < pipeline->num_cmds; i++) {
-        waitpid(pids[i], NULL, 0);
-    }
+	for (i = 0; i < pipeline->num_cmds; i++)
+		waitpid(pids[i], NULL, 0);
 
-    free(pids);
-    return 0;
+	free(pids);
+	return 0;
 }
