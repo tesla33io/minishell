e42e0444c86efefa5b76f4593beb6f0de083295f
Lima <me>
2024-06-03 04:56:43 +0200

    chore: correct word token to str token in grammar

--- include/grammar.h
+++ include/grammar.h
@@ -4,6 +4,6 @@
 #define COMPLETE_COMMAND "pipe_sequence | pipe_sequence AND complete_command | pipe_sequence OR complete_command"
 #define PIPE_SEQUENCE "command | command PIPE pipesequence"
 #define COMMAND "simple_command | L_BRACE complete_command R_BRACE"
-#define SIMPLE_COMMAND* "WORD | HEREDOC WORD | APPEND WORD | IN_REDIRECT WORD | OUT_REDIRECT WORD | WORD simple_command| HEREDOC WORD simple_command | APPEND WORD simple_command| IN_REDIRECT WORD simple_command | OUT_REDIRECT WORD simple_command"
+#define SIMPLE_COMMAND* "STR | HEREDOC STR | APPEND STR | IN_REDIRECT STR | OUT_REDIRECT STR | STR simple_command | HEREDOC STR simple_command | APPEND STR simple_command| IN_REDIRECT STR simple_command | OUT_REDIRECT STR simple_command"
 
 #endif
