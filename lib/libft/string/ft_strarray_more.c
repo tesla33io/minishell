c3576824ce418ebed560ce96e27d1712f8dae006
tesla33io <artemstavropolov@gmail.com>
2024-06-24 21:23:02 +0200

    feat: add strarray function to remove element by value and index

---
+++ lib/libft/string/ft_strarray_more.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strarray_more.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astavrop <astavrop@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/24 21:15:45 by astavrop          #+#    #+#             */
+/*   Updated: 2024/06/24 21:17:22 by astavrop         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+char	**ft_strarray_remove_by_value(char **array, char *str)
+{
+	size_t	len;
+	size_t	i;
+	size_t	j;
+	char	**new_array;
+
+	len = ft_strarray_len(array);
+	new_array = ft_malloc(NULL, (sizeof(char *)) * len);
+	if (!new_array)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (array[i])
+	{
+		if (ft_strcmp(array[i], str) != 0)
+		{
+			new_array[j] = ft_strdup(array[i]);
+			if (!new_array[j])
+			{
+				ft_strarray_free(new_array);
+				return (NULL);
+			}
+			j++;
+		}
+		i++;
+	}
+	new_array[j] = NULL;
+	ft_strarray_free(array);
+	return (new_array);
+}
+
+char	**ft_strarray_remove_by_index(char **array, size_t index)
+{
+	size_t	len;
+	size_t	i;
+	size_t	j;
+	char	**new_array;
+
+	len = ft_strarray_len(array);
+	if (index >= len)
+		return (array);
+
+	new_array = ft_malloc(NULL, (sizeof(char *)) * len);
+	if (!new_array)
+		return (NULL);
+
+	i = 0;
+	j = 0;
+	while (array[i])
+	{
+		if (i != index)
+		{
+			new_array[j] = ft_strdup(array[i]);
+			if (!new_array[j])
+			{
+				ft_strarray_free(new_array);
+				return (NULL);
+			}
+			j++;
+		}
+		i++;
+	}
+	new_array[j] = NULL;
+	ft_strarray_free(array);
+	return (new_array);
+}
