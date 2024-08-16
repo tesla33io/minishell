65feb64402e9db82370e38ffb4a47f060fd04c62
tesla33io <artemstavropolov@gmail.com>
2024-08-16 23:46:22 +0200

    Feat: error when too many arguments

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: astavrop <astavrop@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 21:24:24 by astavrop          #+#    #+#             */
-/*   Updated: 2024/08/14 21:34:28 by astavrop         ###   ########.fr       */
+/*   Updated: 2024/08/16 22:53:04 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,11 @@ int	cd_builtin(t_command *cd)
 
 	if (!cd->args[1])
 		return (builtin_failed(BUILTIN_FAILED, "cd"));
+	if (cd->args && cd->args[1] && cd->args[2])
+	{
+		ft_putendl_fd("cd: too many arguments", 2);
+		return (1);
+	}
 	err = 0;
 	old_pwd = getcwd(NULL, PATH_MAX);
 	if (!old_pwd && ++err)
